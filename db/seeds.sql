3966353042cc32a4ede7ddac797a457c1e1a1a15
Kevin Lee <lee.kev1991@gmail.com>
2017-04-24 17:30:30 -0400

    Added all functionalities

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers VALUES (id, "Malibu Bacon Burger", false, now());
+INSERT INTO burgers VALUES (id, "Sunny Side Burger", false, now());
+INSERT INTO burgers VALUES (id, "Bulgogi Burger", false, now());
